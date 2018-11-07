f5ab23eb22836c62081e274c8d1a02e0680bd88d
Taras Turchenko <taras.turchenko@masterofcode.com>
2018-11-07 21:44:12 +0200

    complete task_2. Refactor task_1

--- ruby_basic/taras.turchenko/homework_1/task_1.rb
+++ ruby_basic/taras.turchenko/homework_1/task_1.rb
@@ -1,20 +1,26 @@
-@timezones = {
-    'America/Anchorage' => '-09:00',
-    'Europe/London' => '+00:00',
-    'Europe/Paris' => '+01:00',
-    'Europe/Kyiv' => '+02:00',
-    'Europe/Minsk' => '+03:00'
+TIMEZONES = {
+    'America/Anchorage': '-09:00',
+    'Europe/London': '+00:00',
+    'Europe/Paris': '+01:00',
+    'Europe/Kyiv': '+02:00',
+    'Europe/Minsk': '+03:00'
 }
 
-def get_time(timezone)
+def get_localtime(timezone)
   time = Time.now
-  format = @timezones[timezone]
-  if timezone == 'UTC'
+
+  if timezone == :UTC
     return time.utc
   end
-  time.localtime format
+
+  if TIMEZONES.key? timezone
+    format = TIMEZONES[timezone]
+    time.localtime format
+  else
+    '[ERROR] Incorrect timezone'
+  end
 end
 
 ARGV.each do |timezone|
-  puts "Time in #{timezone}: #{get_time timezone}"
+  puts "Time in #{timezone}: #{get_localtime timezone.to_sym}"
 end
---
+++ ruby_basic/taras.turchenko/homework_1/task_2/task_2.rb
@@ -0,0 +1,23 @@
+COLORS = {
+    black: 30,
+    red: 31,
+    green: 32,
+    brown: 33,
+    blue: 34,
+    magenta: 35,
+    cyan: 36,
+    gray: 37
+}
+
+def colorize(string, color)
+  "\e[#{COLORS[color]}m#{string}\e[0m"
+end
+
+color = ARGV.first
+path_to_file = ARGV[1]
+
+File.open path_to_file do |file|
+  file.each do |line|
+    print colorize line, color.to_sym
+  end
+end
