9b15f89386aa474b1d091db2f40f0162b5fb26f0
TarasTurchenko <taras.turchenko@masterofcode.com>
2018-11-08 22:29:09 +0200

    resolve issues in task_7

--- ruby_basic/taras.turchenko/homework_1/task_7.rb
+++ ruby_basic/taras.turchenko/homework_1/task_7.rb
@@ -4,11 +4,4 @@ def parse_argv_array(value)
 end
 
 array = parse_argv_array ARGV.first
-
-if array.empty?
-  puts 'Array is empty'
-else
-  random_max = array.length - 1
-  random_element = array[rand(0..random_max)]
-  puts "Random element of array is #{random_element}"
-end
+puts !array.empty? ? "Random element of array is #{array.sample}" : 'Array is empty'
