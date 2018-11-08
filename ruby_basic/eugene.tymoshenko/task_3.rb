74832bddfb73e1e059aa3b0fa5ce75b4d5a181fc
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2018-11-08 14:32:49 +0200

    Update task 3

--- ruby_basic/eugene.tymoshenko/task_3.rb
+++ ruby_basic/eugene.tymoshenko/task_3.rb
@@ -1,6 +1,6 @@
 # Скрипт приймає два масиви та виводить їх спільні елементи
 # Input example: ruby yourscript.rb
 
-arrayFirst = gets.chomp.split
-arraySecond = gets.chomp.split
-print arrayFirst & arraySecond
+array_first = gets.chomp.split
+array_second = gets.chomp.split
+print array_first & array_second
