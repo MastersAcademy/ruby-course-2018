111d04d0968b94a80961d48e1334914a2f0528ee
VladZelenko <Vlad6555@ukr.net>
2018-11-08 22:47:32 +0200

    Update task4.rb

--- ruby_basic/Vlad.Zelenko/HW1/task4.rb
+++ ruby_basic/Vlad.Zelenko/HW1/task4.rb
@@ -2,11 +2,12 @@ require 'objspace'
 
 bool = true
 # bool = gets.chomp
-bool = if bool == "true" || true
-  1
-else 
-  0
-end
+bool = 
+  if bool == "true" || true
+    1
+  else 
+    0
+  end
 
 p ObjectSpace.memsize_of(bool.to_i)
 p ObjectSpace.memsize_of(bool.to_s)
