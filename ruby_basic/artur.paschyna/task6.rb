65794b957ea8c86507cbb8aea3a4db647fa75f63
Артур Пащина <fullwreck@gmail.com>
2018-11-09 00:24:18 +0200

    completed task 6

---
+++ ruby_basic/artur.paschyna/task6.rb
@@ -0,0 +1,13 @@
+class Task1
+  if ARGV[0]=="sum"
+    puts ARGV[1].to_i+ARGV[2].to_i
+  elsif ARGV[0]=="diff"
+    puts ARGV[1].to_i-ARGV[2].to_i
+  elsif ARGV[0]=="mult"
+    puts ARGV[1].to_i*ARGV[2].to_i
+  elsif ARGV[0]=="div"
+    puts ARGV[1].to_i/ARGV[2].to_i
+  else
+    puts "Try again =)"
+  end
+end
