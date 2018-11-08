e7061354d71c26e4882a7f9480771ccef68b5a36
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2018-11-08 17:13:37 +0200

    add task 4

---
+++ ruby_basic/eugene.tymoshenko/task_4.rb
@@ -0,0 +1,9 @@
+#Скрипт приймає boolean, перетворює його в integer, в string, виводить результат та об'єм пам'яті, який кожен з результатів займає
+#Input example: ruby yourscript.rb
+
+boolean = gets.chomp.eql?('true') ? true : false
+integer = boolean && 1 || 0
+string = integer.to_s
+
+puts "result of transformation: int = #{integer}, string = #{string} " +
+  "memsize for: int #{integer.size}, string: #{string.bytesize}"
