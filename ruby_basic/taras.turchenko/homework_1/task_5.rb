a9e0868bcace92ca54591837b390131e8070c1b3
TarasTurchenko <taras.turchenko@masterofcode.com>
2018-11-08 21:48:17 +0200

    resolve issues in task_5

--- ruby_basic/taras.turchenko/homework_1/task_5.rb
+++ ruby_basic/taras.turchenko/homework_1/task_5.rb
@@ -2,9 +2,5 @@ file_path, searched_line = ARGV
 
 File.open file_path do |file|
   index = file.find_index(searched_line + "\n")
-  if index.nil?
-    puts 'Not found line'
-  else
-    puts index
-  end
+  puts index || 'Not found line'
 end
