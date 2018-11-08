0e2b2c3ceeadc29dc05db3d8382e0974bbb509cc
EugeneTymoshenko <jack.tymoshenko@gmail.com>
2018-11-08 18:28:43 +0200

    update code style

--- ruby_basic/eugene.tymoshenko/task_4.rb
+++ ruby_basic/eugene.tymoshenko/task_4.rb
@@ -1,5 +1,5 @@
-#Скрипт приймає boolean, перетворює його в integer, в string, виводить результат та об'єм пам'яті, який кожен з результатів займає
-#Input example: ruby yourscript.rb
+# Скрипт приймає boolean, перетворює його в integer, в string, виводить результат та об'єм пам'яті, який кожен з результатів займає
+# Input example: ruby yourscript.rb
 
 boolean = gets.chomp.eql?('true') ? true : false
 integer = boolean && 1 || 0
