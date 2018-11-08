f04c8e9993d7bd73f1e16497e06e3c2814fef46d
Eugene <bekinassoff@gmail.com>
2018-11-08 12:04:27 +0200

    If-else statement corrected

--- ruby_basic/eugene.doroshenko/task4.rb
+++ ruby_basic/eugene.doroshenko/task4.rb
@@ -4,20 +4,24 @@ class BooleanToStrInt
 	
 	def initialize
 		puts "Input 'true' or 'false'"
-		@boolean_string = gets.chomp
-		@bool_to_int = @boolean_string == 'true' ? 1 : 0
-
+		@bln_string = gets.chomp
+		if @bln_string == 'true' || @bln_string =='1'
+			@bln_to_int = 1
+		else
+			@bln_to_int = 0
+		end
 	end
 
 	def boolean_size_str
-		mem_size_str = ObjectSpace.memsize_of(@boolean_string)
+		mem_size_str = ObjectSpace.memsize_of(@bln_string)
 	end
 
 	def boolean_size_int
-		mem_size_str = ObjectSpace.memsize_of(@bool_to_int)
+		mem_size_str = ObjectSpace.memsize_of(@bln_to_int)
 	end
 
 	start_boolean = BooleanToStrInt.new
+
 
 	puts "Memory size of string type: #{start_boolean.boolean_size_str}"	
 	puts "Memory size of integer type: #{start_boolean.boolean_size_int}"
