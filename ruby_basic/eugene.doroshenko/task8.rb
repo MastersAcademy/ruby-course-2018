5fb16a121c5d6912b5aa80a22b495c6d2bdc81bd
Eugene <bekinassoff@gmail.com>
2018-11-09 14:24:39 +0200

    Objct ArySort vreates behind class. Deleted unused variavle

--- ruby_basic/eugene.doroshenko/task8.rb
+++ ruby_basic/eugene.doroshenko/task8.rb
@@ -8,10 +8,10 @@ class ArySort
 	def str_to_array
 		arr = Array.new
 		arr = @arr_string.split(" ")
-		array_sorted = arr.sort.reverse
+		arr.sort.reverse
 	end
 
-	start_string = ArySort.new
-	puts "Sorted Array is #{start_string.str_to_array}"
-
 end
+
+start_string = ArySort.new
+puts "Sorted Array is #{start_string.str_to_array}"
