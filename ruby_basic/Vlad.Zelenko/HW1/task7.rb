a0c7e2e36eade2b3ce7322d8d8dc515837a3c22d
VladZelenko <Vlad6555@ukr.net>
2018-11-09 15:19:44 +0200

    Update task7.rb

--- ruby_basic/Vlad.Zelenko/HW1/task7.rb
+++ ruby_basic/Vlad.Zelenko/HW1/task7.rb
@@ -1,9 +1,1 @@
-# p [1,4,6,7,8,9,23,6,56].sample
-class RandNumberArray
-  def randNumbArr(*arr)
-    arr.sample
-  end
-end
-
-rand_numb = RandNumberArray.new
-p rand_numb.randNumbArr(1,4,6,7,8,9,23,6,56)
+puts gets.chomp.split(",").sample
