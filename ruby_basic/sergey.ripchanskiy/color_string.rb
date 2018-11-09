ffb91d6fc81a215de567aaff5cd587b266e90203
Sergey <sergeyripchanskiy@gmail.com>
2018-11-09 13:31:56 +0200

    Adjustment color_string function, add only one method to String class, create initialize method, delete puts method from method, use constant

--- ruby_basic/sergey.ripchanskiy/color_string.rb
+++ ruby_basic/sergey.ripchanskiy/color_string.rb
@@ -1,40 +1,33 @@
 # TASK #2 Receive text file and color and input colored to console
-class String
-  def recieve_data
+COLORS = {
+  red: 31,
+  green: 32,
+  yellow: 33,
+  blue: 34,
+  pink: 35,
+  light_blue: 36
+}.freeze
+
+# Get params(color and text) and pass to method colorize of String class
+class Color
+  def initialize
     @params = ARGV
-    @color = @params[0]
-    @text_file = @params[1]
-    @text_file = File.open(@text_file).read
-    puts @text_file.send(@color.to_sym)
   end
 
+  def colorator
+    color = @params[0].to_sym
+    color_code = COLORS[color]
+    get_text_file = @params[1]
+    text_file = File.open(get_text_file).read
+    text_file.colorize(color_code)
+  end
+end
+
+# Add colorize method to string class
+class String
   def colorize(color_code)
     "\e[#{color_code}m#{self}\e[0m"
   end
-
-  def red
-    colorize(31)
-  end
-
-  def green
-    colorize(32)
-  end
-
-  def yellow
-    colorize(33)
-  end
-
-  def blue
-    colorize(34)
-  end
-
-  def pink
-    colorize(35)
-  end
-
-  def light_blue
-    colorize(36)
-  end
 end
-String.new.recieve_data
-# type ruby color_string.rb pink text.txt
+puts Color.new.colorator
+# type ruby test.rb pink text.txt
