e611c07d201d810d9e5781ee77be3f892daf53df
Sergey <sergeyripchanskiy@gmail.com>
2018-11-09 16:15:06 +0200

    use methodes ach_with_index and readlines

--- ruby_basic/sergey.ripchanskiy/find_textline.rb
+++ ruby_basic/sergey.ripchanskiy/find_textline.rb
@@ -7,12 +7,10 @@ class FindLine
   def find_line
     text_file_name = @params[0]
     text = @params[1]
-    line_array = File.open(text_file_name).read.split("\n")
-    @counter = 0
-    line_array.each do |item|
-      @counter += 1
-      puts "The number of line is: #{@counter}" if item[/#{text}/]
-      break if item[/#{text}/]
+    line_array = File.readlines(text_file_name)
+    line_array.each_with_index do |val,index|
+      puts "The number of line is: #{index + 1}" if val[/#{text}/]
+      break if val[/#{text}/]
     end
   end
 end
