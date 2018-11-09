a01cad17d1a09066badce86333a14b42e128ff73
Sergey <sergeyripchanskiy@gmail.com>
2018-11-09 13:54:45 +0200

    fix task4

--- ruby_basic/sergey.ripchanskiy/data_type_size.rb
+++ ruby_basic/sergey.ripchanskiy/data_type_size.rb
@@ -1,16 +1,15 @@
 # TASK #4 Show boolean as string and integer size
 input_data = ARGV[0]
 def to_boolean(input_data)
-  case input_data
-    when true,'true',1,'1'
-      return true
-    when false, 'false',0,'0'
-      return false
+  if input_data == 'true'
+    1
+  else
+    0
   end
 end
 
-boolean_as_string_size =  to_boolean(input_data).to_s.bytesize
+boolean_as_string_size = to_boolean(input_data).to_s.bytesize
 puts "boolean as string size = #{boolean_as_string_size} bytes"
-boolean_as_integer_size =  to_boolean(input_data).to_s.to_i.size
+boolean_as_integer_size = to_boolean(input_data).size
 puts "boolean as integer size = #{boolean_as_integer_size} bytes"
-# type ruby data_type_size.rb 1 / true / 'true'
+# type ruby data_type_size.rb  true / false
