9c72b8b001e99fc236757db6d8ffe6108f791aaa
VladZelenko <Vlad6555@ukr.net>
2018-11-09 15:14:16 +0200

    Update task6.rb

--- ruby_basic/Vlad.Zelenko/HW1/task6.rb
+++ ruby_basic/Vlad.Zelenko/HW1/task6.rb
@@ -1,14 +1,14 @@
 class CalcNumbers
-  def sum(*arr)
-    arr.inject(0) {|sum, num|  sum + num }
+  def sum(a = gets.to_i, b = gets.to_i)
+    a + b
   end
 
   def subtraction(a = gets.to_i, b = gets.to_i)
     a - b
   end
   
-  def multiplication(*arr)
-    arr.inject(1) {|a, num|  a * num }
+  def multiplication(a = gets.to_i, b = gets.to_i)
+    a * b
   end
   
   def division(a = gets.to_i, b = gets.to_i)
@@ -17,7 +17,7 @@ class CalcNumbers
 end
 
 calc = CalcNumbers.new
-# p calc.sum(1,5,6,7,8)
+p calc.sum
 # p calc.subtraction
-# p calc.multiplication(2,2)
+# p calc.multiplication
 # p calc.division
