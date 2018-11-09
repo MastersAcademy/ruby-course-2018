c0b64759d5ebcfb24c8a1ce9f1be2cbf1833078e
Sergey <sergeyripchanskiy@gmail.com>
2018-11-09 14:05:44 +0200

    fix problem with class variables in hw5

--- ruby_basic/sergey.ripchanskiy/find_textline.rb
+++ ruby_basic/sergey.ripchanskiy/find_textline.rb
@@ -1,18 +1,20 @@
 # TASK #5. Find the text line from text file
 class FindLine
-  @params = ARGV
-  @text_file_name = @params[0]
-  @text = @params[1]
+  def initialize
+    @params = ARGV
+  end
 
-  def self.find_line
-    line_array = File.open(@text_file_name).read.split("\n")
+  def find_line
+    text_file_name = @params[0]
+    text = @params[1]
+    line_array = File.open(text_file_name).read.split("\n")
     @counter = 0
     line_array.each do |item|
       @counter += 1
-      puts "The number of line is: #{@counter}" if item[/#{@text}/]
-      break if item[/#{@text}/]
+      puts "The number of line is: #{@counter}" if item[/#{text}/]
+      break if item[/#{text}/]
     end
   end
 end
-FindLine.find_line
+FindLine.new.find_line
 # type ruby find_textline.rb text.txt battle
