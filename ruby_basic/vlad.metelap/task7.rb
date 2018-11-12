65a0d3e6a1826a25828b5bd70a58416f6a8827ef
westnick2 <vmetw@ukr.net>
2018-11-12 09:13:54 +0200

    task2 task3 refactored

--- ruby_basic/vlad.metelap/task7.rb
+++ ruby_basic/vlad.metelap/task7.rb
@@ -2,9 +2,7 @@ class Task7
 
   def self.arr_random(str_arr)
     arr = str_arr.split(",")
-    arr_size = arr.length
-    rand_num = rand(arr_size)
-    arr[rand_num]
+    random_element = arr.sample
   end
 end
 str_arr = ARGV[0]
