53af8bd7b6aea024c03999e1ee0d01cfa2373bc5
MrGaoler <notwronglife@gmail.com>
2018-11-12 22:39:27 +0200

    tab correction in the sixth task

--- ruby_basic/andrii.gluschak/task#6.rb
+++ ruby_basic/andrii.gluschak/task#6.rb
@@ -5,14 +5,14 @@ second_value = gets.chomp.to_i
 p "enter plus or minus, sum or div"
 v = gets.chomp
 result =
-case v
-when "plus"
-  first_value + second_value
-when "minus"
-  first_value - second_value
-when "sum"
-  first_value * second_value
-when "div"
-  first_value / second_value
-end
+  case v
+  when "plus"
+    first_value + second_value
+  when "minus"
+    first_value - second_value
+  when "sum"
+    first_value * second_value
+  when "div"
+    first_value / second_value
+  end
 p "final result of #{v} is #{result}"
