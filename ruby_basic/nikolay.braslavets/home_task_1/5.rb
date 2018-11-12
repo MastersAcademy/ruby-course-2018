341861ffb620a9ceb15a21c73ce02446f5adb658
daglus <daglus.ck.ua@gmail.com>
2018-11-12 13:27:09 +0200

    Add task solution #5

--- ruby_basic/nikolay.braslavets/home_task_1/5.rb
+++ ruby_basic/nikolay.braslavets/home_task_1/5.rb
@@ -1,1 +1,24 @@
-#Not ready
+filename = ARGV[0]
+word = ARGV[1]
+
+unless ARGV.size == 2
+  puts 'Wrong argumet'
+  exit
+end
+
+unless File.exists?(filename)
+  puts 'File not found'
+  exit
+end
+
+count = 0
+File.open(filename, "r") do |f|
+  f.each do |line|
+    count +=1
+    if line.match(/\b#{word}\b/i)
+      puts "#{word} was found on line #{count}"
+      exit
+    end
+ end
+ puts 'not found this word'
+end
