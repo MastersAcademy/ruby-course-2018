9a87fca4ea716f7fc9b130b2babf5f78b2f2a754
VladZelenko <Vlad6555@ukr.net>
2018-11-13 23:55:29 +0200

    Update task6.rb

--- ruby_basic/Vlad.Zelenko/HW1/task6.rb
+++ ruby_basic/Vlad.Zelenko/HW1/task6.rb
@@ -1,23 +1,60 @@
-class CalcNumbers
-  def sum(a = gets.to_i, b = gets.to_i)
-    a + b
-  end
+# class CalcNumbers
+#   attr_reader :a, :b
 
-  def subtraction(a = gets.to_i, b = gets.to_i)
-    a - b
-  end
-  
-  def multiplication(a = gets.to_i, b = gets.to_i)
-    a * b
-  end
-  
-  def division(a = gets.to_i, b = gets.to_i)
-    a.to_f / b
-  end
-end
+#   def initialize(a, b)
+#     @a = a.to_i
+#     @b = b.to_i
+#   end
 
-calc = CalcNumbers.new
-p calc.sum
+#   def sum
+#     a + b
+#   end
+
+#   def subtraction
+#     a - b
+#   end
+  
+#   def multiplication
+#   end
+  
+#   def division
+#     a.to_f / b
+#   end
+# end
+
+# calc = CalcNumbers.new(gets, gets)
+# p calc.sum
 # p calc.subtraction
 # p calc.multiplication
 # p calc.division
+
+
+class CalcNumbers
+  def initialize(arr)
+    @arr = arr
+    @a = @arr[0].to_i
+    @b = @arr[1].to_i
+  end
+
+  def sum
+    @a + @b
+  end
+
+  def subtraction
+    @a - @b
+  end
+  
+  def multiplication
+    @a * @b
+  end
+  
+  def division
+    @a.to_f / @b
+  end
+end
+
+calc = CalcNumbers.new(ARGV)
+p calc.sum
+p calc.subtraction
+p calc.multiplication
+p calc.division
