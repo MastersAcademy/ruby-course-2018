00b768fc256e87fdeb879a394336706c585a7b98
st3p <androidmarketchrome@gmail.com>
2018-11-14 18:56:46 +0200

    Task #7 done

---
+++ objects/vazgen.bakhchagulian/task7.rb
@@ -0,0 +1,34 @@
+module Brackets
+  def parentheses_valid?
+    stack = []
+    mapping = {
+        ")" => "(",
+        "}" => "{",
+        "]" => "[",
+        # ">" => "<"
+    }
+
+    self.each_char do |char|
+      if mapping.has_key?(char)
+        if !stack.empty?
+          top_element = stack.pop
+        else
+          top_element = '#'
+        end
+        if mapping[char] != top_element
+          return false
+        end
+      else
+        stack.push(char)
+      end
+    end
+
+    return stack.empty?
+  end
+end
+
+class String
+  include Brackets
+end
+
+puts ARGV[0].parentheses_valid? ? "valid" : "invalid"
