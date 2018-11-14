26d142ac9afaf2a32d703a3e953cd978da884001
Vlad6555 <Vlad6555@ukr.net>
2018-11-14 22:34:03 +0200

    task1, task2

---
+++ objects/vlad.zelenko/task1/new_game.rb
@@ -0,0 +1,22 @@
+require_relative "output_methods"
+
+class NewGame
+  include OutPutMethods
+
+  def initialize(level_number, background, speed, name)
+    @level_number = level_number
+    @background = background
+    @speed_enemies = speed
+    @name = name
+  end
+
+  def game_method
+    "Hello, I am method #{@name}"
+  end
+
+end 
+
+p NewGame.new(6,7,8, "One").game_method
+p NewGame.new(6,7,8, "One").show_level_number
+p NewGame.new(6,7,8, "One").background
+p NewGame.new(6,7,8, "One").show_speed_enemies
