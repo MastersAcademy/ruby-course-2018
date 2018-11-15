17a46ea22b3e2c40ed2f78671a61ecc4d43db414
TarasTurchenko <taras.turchenko@masterofcode.com>
2018-11-15 14:16:04 +0200

    complete task 1

---
+++ objects/taras.turchenko/task_1/base_game.rb
@@ -0,0 +1,13 @@
+module BaseGame
+  def show_background
+    "Background of level is #{@background}"
+  end
+
+  def show_speed_enemies
+    "Speed of enemies on level is #{@speed_enemies}"
+  end
+
+  def show_level_number
+    "Current level is #{@level_number}"
+  end
+end
---
+++ objects/taras.turchenko/task_1/the_witcher.rb
@@ -0,0 +1,22 @@
+require './task_1/base_game'
+
+class TheWitcher
+  include BaseGame
+
+  attr_accessor :level_background, :speed_enemies, :level_number, :geralt_level
+
+  def initialize(level_background, speed_enemies, level_number, geralt_level)
+    @level_background = level_background
+    @speed_enemies = speed_enemies
+    @level_number = level_number
+    @geralt_level = geralt_level
+  end
+
+  def show_geralt_level
+    "Geralt of Rivia has #{@geralt_level} level"
+  end
+
+  def joke_about_unicorns
+    "It's a long story..."
+  end
+end
