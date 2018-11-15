ba90c5ceaf8ddd69b46bc5f1e349123288a98d90
Артур Пащина <fullwreck@gmail.com>
2018-11-15 20:23:42 +0200

    Updated task6

--- ruby_basic/artur.paschyna/task6.rb
+++ ruby_basic/artur.paschyna/task6.rb
@@ -1,12 +1,17 @@
-operation, first_value, second_value = ARGV
-  if operation=="sum"
-    puts first_value.to_i+second_value.to_i
-  elsif operation=="diff"
-    puts first_value.to_i-second_value.to_i
-  elsif operation=="mult"
-    puts first_value.to_i*second_value.to_i
-  elsif operation=="div"
-    puts first_value.to_i/second_value.to_i
-  else
-    puts "Try again =)"
+class Task6
+  def find_the_result(operation, first_value, second_value)
+    if operation == "sum"
+      puts first_value.to_i + second_value.to_i
+    elsif operation == "diff"
+      puts first_value.to_i - second_value.to_i
+    elsif operation == "mult"
+      puts first_value.to_i * second_value.to_i
+    elsif operation == "div"
+      puts first_value.to_i / second_value.to_i
+    else
+      puts "Try again =)"
+    end
   end
+end
+ftr = Task6.new
+ftr.find_the_result(ARGV[0],ARGV[1],ARGV[2])
