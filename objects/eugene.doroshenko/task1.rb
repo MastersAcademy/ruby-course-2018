3fa2ff872a7b25a2e385996553a32f532123dd2a
Eugene <bekinassoff@gmail.com>
2018-11-15 16:08:43 +0200

    Added MarioGame code

--- objects/eugene.doroshenko/task1.rb
+++ objects/eugene.doroshenko/task1.rb
@@ -8,3 +8,34 @@
 #  include SomeModule
 #end
 #Далі зробити класс для іншої гри з будь якою іншою назвою, і та гра має містити свої перстональні методи, і так само методи які використовує MarioGame. Тут треба реалізувати не наслідуванням а саме підключенням модуля із необхідними методами.
+
+class MarioGame
+  attr_accessor :level_background, :count_of_enemies, :speed_enemies, :level_number
+
+  def initialize(level_background, count_of_enemies, speed_enemies, level_number)
+    @background = level_background
+    @enemies = count_of_enemies
+    @speed_enemies = speed_enemies
+    @level_number = level_number
+  end
+
+  def change_level(new_level_number)
+    @level_number = new_level_number
+  end
+
+  def change_background(new_background_color)
+    @background = new_background_color
+  end
+
+  def change_speed_enemies(new_speed_enemies)
+    @speed_enemies = new_speed_enemies
+  end
+
+  def change_counf_of_enemies(new_count_of_enemies)
+    @enemies = new_count_of_enemies
+  end
+
+  def show_count_of_enemies
+    "Count of enemies on level is: #{@enemies}"
+  end
+end
