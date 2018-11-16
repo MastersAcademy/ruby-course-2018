f439c7ed68e785fd7a65a84f487b7fddc00f3af8
Eugene <bekinassoff@gmail.com>
2018-11-16 12:27:01 +0200

    task 1 HW2

--- objects/eugene.doroshenko/task1.rb
+++ objects/eugene.doroshenko/task1.rb
@@ -9,16 +9,8 @@
 #end
 #Далі зробити класс для іншої гри з будь якою іншою назвою, і та гра має містити свої перстональні методи, і так само методи які використовує MarioGame. Тут треба реалізувати не наслідуванням а саме підключенням модуля із необхідними методами.
 
-class MarioGame
-  attr_accessor :level_background, :count_of_enemies, :speed_enemies, :level_number
-
-  def initialize(level_background, count_of_enemies, speed_enemies, level_number)
-    @background = level_background
-    @enemies = count_of_enemies
-    @speed_enemies = speed_enemies
-    @level_number = level_number
-  end
-
+module GamesMethods
+  
   def change_level(new_level_number)
     @level_number = new_level_number
   end
@@ -36,6 +28,82 @@ class MarioGame
   end
 
   def show_count_of_enemies
-    "Count of enemies on level is: #{@enemies}"
+    puts "Count of enemies on level is: #{@enemies}"
   end
+
+  def show_enemy_speed
+    puts "Anemy speed is: #{@speed_enemies}"
+  end
+
+  def show_level
+    puts "Level #{@level_number}"
+  end
+
+  def show_background
+    puts "Level background #{@background}"
+  end
+
+end
+
+# end of module MariomMethods
+
+#============================================================================
+
+#class MarioGame
+class MarioGame
+  
+  attr_accessor :level_background, :count_of_enemies, :speed_enemies, :level_number
+  include GamesMethods
+
+  def initialize(level_background, count_of_enemies, speed_enemies, level_number)
+    @background = level_background
+    @enemies = count_of_enemies
+    @speed_enemies = speed_enemies
+    @level_number = level_number
+  end
+
+  lvl_bground = 1
+  cnt_of_enems = 15
+  spd_enems = 1 
+  lvl_number = 1
+
+  TanksGame.new(lvl_bground, cnt_of_enems, spd_enems, lvl_number)
+
+end
+
+
+#end of MarioGame class
+#==========================================
+#enother game
+
+class TanksGame
+
+  attr_accessor :level_background, :count_of_enemies, :speed_enemies, :level_number, :level_map
+
+  include GamesMethods
+
+  def initialize(level_background, count_of_enemies, speed_enemies, level_number, level_map)
+    @lev_map = level_map
+    @background = level_background
+    @enemies = count_of_enemies
+    @speed_enemies = speed_enemies
+    @level_number = level_number
+  end
+
+  def level_map (new_level_map)
+    @lev_map = new_count_of_enemies
+  end
+
+  def show_level_map
+    "Level map is: #{@lev_map}"
+  end
+
+  lvl_bground = 1
+  cnt_of_enems = 15
+  spd_enems = 1 
+  lvl_number = 1
+  lvl_map = 1
+
+  TanksGame.new(lvl_bground, cnt_of_enems, spd_enems, lvl_number, lvl_map)
+
 end
