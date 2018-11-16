1698b2de7ad6becc7c40f253440c1f3e1a707e33
VladZelenko <Vlad6555@ukr.net>
2018-11-16 12:07:01 +0200

    Update task3.rb

--- objects/vlad.zelenko/hw1/task3.rb
+++ objects/vlad.zelenko/hw1/task3.rb
@@ -1,9 +1,4 @@
-first_arr = gets.chomp.split(",")
-second_arr = gets.chomp.split(",")
-
-
-def arr_common_elem(a, b)
-  a & b
-end
-
-p arr_common_elem(first_arr, second_arr)
+require 'csv'
+arr1 = gets.chomp.parse_csv
+arr2 = gets.chomp.parse_csv
+arr3 = arr1 & arr2
