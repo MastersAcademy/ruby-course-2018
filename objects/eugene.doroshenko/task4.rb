181bdf48a6a7e94ff9955856a3634b321cfa0f9f
Eugene <bekinassoff@gmail.com>
2018-11-16 15:42:40 +0200

    Task 4. Not work. need to be fixed

--- objects/eugene.doroshenko/task4.rb
+++ objects/eugene.doroshenko/task4.rb
@@ -1,2 +1,27 @@
 
 #Реалізувати клас для математичних операцій. Методи повинні декларуватись динамічно.
+class MathOperations
+
+   OPERATORS = {
+      plus: '+',
+      minus: '-',
+      divide: '/',
+      myltiply: '*'
+  }.freeze
+
+  def initialize
+    puts "Input first element"
+    @el1 = gets.chomp.to_i
+    puts "Input second element"
+    @el2 = gets.chomp.to_i
+  end
+
+  OPERATORS.each do |method_name, math_symbol|
+    define_method method_name do
+      puts "#{@el1.send(math_symbol, @el2)}" 
+    end
+  end
+
+end
+
+MathOperations.new
