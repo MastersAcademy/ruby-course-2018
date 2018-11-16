73bb11ccbe5d53cd17612eda60d19d2a831ac656
Vlad6555 <Vlad6555@ukr.net>
2018-11-16 20:37:52 +0200

    add fix hw1 tasks 3-8 add hw2 tasks 1-4

--- objects/vlad.zelenko/task3/even_numb.rb
+++ objects/vlad.zelenko/task3/even_numb.rb
@@ -8,5 +8,5 @@ class EvenNumb
   end
 end
 
-even_numb = EvenNumb.new([1, 2, 3, 4, "two", "seven", 5, 6, 8, 9])
+even_numb = EvenNumb.new([0, 1, 2, 3, 4, "two", "seven", 5, 6, 8, 9])
 even_numb.out_even_numb
