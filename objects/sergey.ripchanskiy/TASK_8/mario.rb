1ef96b71b6bd09bc49a808aed261ba95e2088ebc
Sergey <sergeyripchanskiy@gmail.com>
2018-11-17 22:39:27 +0200

    delete line-variables

--- objects/sergey.ripchanskiy/TASK_8/mario.rb
+++ objects/sergey.ripchanskiy/TASK_8/mario.rb
@@ -30,26 +30,26 @@ class Mario
 
     100.times do print '#' end
     puts ''
-    line1 = repeat(' ', 100) if !final_stage?
-    line1 = repeat(' ', 80) + repeat('#', 3) if final_stage?
+    repeat(' ', 100) if !final_stage?
+    repeat(' ', 80) + repeat('#', 3) if final_stage?
     puts ''
-    line2 = repeat(' ', 100) if !final_stage?
-    line2 = repeat(' ', 80) + repeat('#', 3) if final_stage?
+    repeat(' ', 100) if !final_stage?
+    repeat(' ', 80) + repeat('#', 3) if final_stage?
     puts ''
-    line3 = repeat(' ', 100) if !final_stage?
-    line2 = repeat(' ', 82) + repeat('#', 1) if final_stage?
+    repeat(' ', 100) if !final_stage?
+    repeat(' ', 82) + repeat('#', 1) if final_stage?
     puts ''
-    line4 = repeat(' ', @steps) + repeat(' ', @space_to_pyramid) + repeat('#', 4)
+    repeat(' ', @steps) + repeat(' ', @space_to_pyramid) + repeat('#', 4)
     puts ''
-    line5 = repeat(' ', @steps) + repeat(' ', @space_to_pyramid - 1) + repeat('#', 6)
+    repeat(' ', @steps) + repeat(' ', @space_to_pyramid - 1) + repeat('#', 6)
     puts ''
-    line6 = repeat(' ', @steps) + repeat(' ', @space_to_pyramid - 2) + repeat('#', 8)
+    repeat(' ', @steps) + repeat(' ', @space_to_pyramid - 2) + repeat('#', 8)
     puts ''
-    line7 = repeat(' ', @steps) + repeat(' ', @space_before_user) + repeat('#', USER_PARTS[:user_top_bottom]) + repeat(' ', @space_to_pyramid - 3 - @bu_space) + repeat('#', 10)
+    repeat(' ', @steps) + repeat(' ', @space_before_user) + repeat('#', USER_PARTS[:user_top_bottom]) + repeat(' ', @space_to_pyramid - 3 - @bu_space) + repeat('#', 10)
     puts ''
-    line8 = repeat(' ', @steps) + repeat(' ', @space_before_user_body) + repeat('#', USER_PARTS[:user_body]) + repeat(' ', @space_to_pyramid - 4 - @bb_space) + repeat('#', 12)
+    repeat(' ', @steps) + repeat(' ', @space_before_user_body) + repeat('#', USER_PARTS[:user_body]) + repeat(' ', @space_to_pyramid - 4 - @bb_space) + repeat('#', 12)
     puts ''
-    line9 = repeat(' ', @steps) + repeat(' ', @space_before_user) + repeat('#', USER_PARTS[:user_top_bottom]) + repeat(' ', @space_to_pyramid - 5 - @bu_space) + repeat('#', 14)
+    repeat(' ', @steps) + repeat(' ', @space_before_user) + repeat('#', USER_PARTS[:user_top_bottom]) + repeat(' ', @space_to_pyramid - 5 - @bu_space) + repeat('#', 14)
     puts ''
     100.times do print '#' end
     puts ''
