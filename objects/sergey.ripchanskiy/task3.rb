fd0ae7aaf0dc629cc260286fcdba781e51c09956
Sergey <sergeyripchanskiy@gmail.com>
2018-11-17 23:35:35 +0200

    fix task3

--- objects/sergey.ripchanskiy/task3.rb
+++ objects/sergey.ripchanskiy/task3.rb
@@ -1,10 +1,10 @@
 # EvenNumbers
 class EvenNumbers
   def select_even_numbers(array)
-    select_numbers = array.select { |item| item.is_a? Integer }
+    select_numbers = array.map(&:to_i)
     select_numbers.select { |number| (number % 2).zero? }
   end
 end
 
-arr = [1, 4, 6, 8, 'Justin Biber', 'Ozzy Osborne', 17]
+arr = [1, 4, 6, 8, '18', '11', 17]
 puts EvenNumbers.new.select_even_numbers(arr)
