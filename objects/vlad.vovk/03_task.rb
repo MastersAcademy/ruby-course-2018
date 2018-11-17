da14f59408e40b2489eb443141ad6643fcc08990
vladvovk2 <vlad.vovk.844@gmail.com>
2018-11-17 10:52:23 +0200

    Refactored task 3

--- objects/vlad.vovk/03_task.rb
+++ objects/vlad.vovk/03_task.rb
@@ -1,6 +1,9 @@
 class A
   def self.even_numbers(array)
-    array.delete 0
-    array.select { |i| i.to_i.even? if i.is_a?(Integer) || i.to_i.nonzero?} 
+    array.select do |i|
+      unless i.is_a? Symbol
+        i.to_i.even? if i.is_a?(Integer) || i.to_i.nonzero?
+      end
+    end
   end
 end
