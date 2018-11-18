76fc6e4b5df66c4793f4ee7015148dad3bb58a31
vladvovk2 <vlad.vovk.844@gmail.com>
2018-11-18 13:29:04 +0200

    Fixed third task && deleted empty 7,8 tasks

--- objects/vlad.vovk/03_task.rb
+++ objects/vlad.vovk/03_task.rb
@@ -1,9 +1,15 @@
 class A
-  def self.even_numbers(array)
-    array.select do |i|
-      unless i.is_a? Symbol
-        i.to_i.even? if i.is_a?(Integer) || i.to_i.nonzero?
-      end
+  class << self
+    def even_numbers(array)
+      array.select { |obj| even_integer?(obj) || even_string?(obj) }
+    end
+
+    def even_integer?(object)
+      object.is_a?(Integer) && object.even?
+    end 
+
+    def even_string?(object)
+      object.is_a?(String) && object.to_i.even?
     end
   end
 end
