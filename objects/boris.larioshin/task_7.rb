6dbc6e1ab2da3d9838296397f71a52d6de0f3ede
boris <isakovbr@gmail.com>
2018-11-18 23:47:21 +0200

    task_7

---
+++ objects/boris.larioshin/task_7.rb
@@ -0,0 +1,34 @@
+# created class BalancedBrackets for task number 7
+class BalancedBrackets
+  OPEN_BRACKETS = '{[(<'.freeze
+  CLOSE_BRACKETS = '}])>'.freeze
+  attr_accessor :str
+
+  def initialize(str)
+    @str = str
+  end
+
+  def brackets_correct
+    stack = []
+    str.each_char do |symbol|
+      if OPEN_BRACKETS.include?(symbol)
+        stack.push(symbol)
+      elsif CLOSE_BRACKETS.include?(symbol)
+        index = CLOSE_BRACKETS.index(symbol)
+        if stack.last == OPEN_BRACKETS[index]
+          stack.pop
+        else
+          return puts 'invalid'
+        end
+      end
+    end
+    if stack.empty?
+      puts 'valid'
+    else
+      puts 'invalid'
+    end
+  end
+end
+
+str_from_terminal = ARGV.first
+BalancedBrackets.new(str_from_terminal).brackets_correct
