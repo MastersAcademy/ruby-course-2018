86391ab017deca43ae3cbb1fbf2b5159435e1f87
westnick2 <vmetw@ukr.net>
2018-11-20 18:02:18 +0200

    resolved errors task 7

--- objects/vlad.metelap/task7.rb
+++ objects/vlad.metelap/task7.rb
@@ -1,31 +1,29 @@
 # task 7
 class Parentheses
   def check_validate(string, parentheses_pairs, parentheses_symbols)
-    arr = string.split("")
-    if parentheses_pairs.has_key?(string[0])
-      array_of_symbols = []
-      arr.each do |x|
-        array_of_symbols << x if parentheses_symbols.include? x
-      end
-
+    arr = string.chars
+    if parentheses_pairs.key?(string[0])
+      array_of_symbols = arr.select {|x| parentheses_symbols.include? x}
       arr_compare_results = []
-      if (array_of_symbols.count % 2).zero?
-        i = 0
-        c = array_of_symbols.length - 1
-        while i < array_of_symbols.count / 2
-          x = array_of_symbols[c] == parentheses_pairs["#{array_of_symbols[i]}"]
-          i += 1
-          c -= 1
-          arr_compare_results << x
+      if array_of_symbols.count.even?
+        first_element_index = 0
+        last_element_index = array_of_symbols.length - 1
+        count_of_iterations = array_of_symbols.count / 2
+        while first_element_index < count_of_iterations
+          opened_bracket = array_of_symbols[first_element_index]
+          compare_elements = array_of_symbols[last_element_index] == parentheses_pairs["#{opened_bracket}"]
+          first_element_index += 1
+          last_element_index -= 1
+          arr_compare_results << compare_elements
         end
 
         if arr_compare_results.include? false
-          puts 'you are wrong in this string'
+          puts 'you are wrong in this string!'
         else
-          puts 'write you are'
+          puts 'write you are!'
         end
       else
-        puts 'The number of parenthese must be a couples'
+        puts 'The number of parenthese must be even!'
       end
 
     else
@@ -33,12 +31,8 @@ class Parentheses
     end
   end
 end
+
 parentheses_pairs = {"(" => ")", "{" => "}", "[" => "]"}
 parentheses_symbols = ["(", ")", "{", "}", "[", "]"]
-
-# Try this strings:
-string = '{(aaa)}'
-# string = '{(aaa)'
-# string = '{(aaa)}'
-
+string = '{(words)}'
 Parentheses.new.check_validate(string, parentheses_pairs, parentheses_symbols)
