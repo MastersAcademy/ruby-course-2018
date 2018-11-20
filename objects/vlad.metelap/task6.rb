9617757fb8be3523d928d8132d013cdf97d40c99
westnick2 <vmetw@ukr.net>
2018-11-20 17:11:41 +0200

    resolved errors task 6

--- objects/vlad.metelap/task6.rb
+++ objects/vlad.metelap/task6.rb
@@ -1,33 +1,46 @@
 # Task 6
+require './extmodule'
+class String
+
+  def black
+    "\e[30m#{self}\e[0m"
+  end
+
+  def red
+    "\e[31m#{self}\e[0m"
+  end
+
+  def green
+    "\e[32m#{self}\e[0m"
+  end
+
+  def brown
+    "\e[33m#{self}\e[0m"
+  end
+
+  def blue
+    "\e[34m#{self}\e[0m"
+  end
+
+end
 
 class Maths
-  OPERATORS = {
-    plus: '+',
-    minus: '-',
-    divide: '/',
-    multiply: '*'
-  }.freeze
-
-  COLORS = {
-    minus: 31,
-    plus: 32,
-    devide: 33,
-    multiply: 34
-  }.freeze
+  include Extmodule
 
   def initialize(a, b)
-    @a, @b = a, b
+    @a = a
+    @b = b
   end
 
   OPERATORS.each do |method_name, operator|
     define_method method_name do
-      colour = COLORS[method_name.to_sym]
+      colour = COLOURS[method_name.to_sym]
       rezult = @a.send(operator, @b)
-      "\e[#{colour}m#{rezult}\e[0m"
+      rezult.to_s.send(colour.to_sym)
+      # "\e[#{colour}m#{rezult}\e[0m"
     end
   end
 end
-
 
 puts Maths.new(9, 47).plus
 puts Maths.new(-2, 140).minus
