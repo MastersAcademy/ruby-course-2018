82202e1a8d56c04b183dc15c79fc4356320178be
Eugene <bekinassoff@gmail.com>
2018-11-20 12:24:27 +0200

    Accessor level_map deleted. Method renamed to change_map. Value name in method corrected

--- objects/eugene.doroshenko/tanks_game.rb
+++ objects/eugene.doroshenko/tanks_game.rb
@@ -2,18 +2,17 @@ require_relative 'games_methods'
 
 class TanksGame
 
-  attr_accessor :level_map
   include GamesMethods
 
   def initialize(level_background, count_of_enemies, speed_enemies, level_number, level_map)
-    @lev_map = level_map
+    @lvl_map = level_map
     @background = level_background
     @enemies = count_of_enemies
     @speed_enemies = speed_enemies
     @level_number = level_number
   end
 
-  def level_map (new_level_map)
+  def change_map(new_level_map)
     @lvl_map = new_level_map
   end
 
@@ -22,5 +21,7 @@ class TanksGame
   end
 
 end
+
+puts TanksGame.new(5, 50, 5, 2, 5).show_level_map
 
 80.times {TanksGame.new(1, 20, 1, 1, 1)}
