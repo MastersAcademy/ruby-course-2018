762336ba45f4bf5080b764c2073163789da03e08
Eugene <bekinassoff@gmail.com>
2018-11-22 16:50:47 +0200

    Tasks 4 and 6 - done

---
+++ objects/eugene.doroshenko/math_operations_list.rb
@@ -0,0 +1,12 @@
+module MathMethodsAndOperators
+
+  def operators_list
+    { 
+      plus: '+',
+      minus: '-',
+      divide: '/',
+      multiply: '*'
+    }.freeze
+  end
+  
+end
---
+++ objects/eugene.doroshenko/task4_6.rb
@@ -0,0 +1,27 @@
+require_relative "math_operations_list"
+
+class MathOperations
+  attr_accessor :el1, :el2
+  extend MathMethodsAndOperators
+
+  def initialize
+    puts "Input first element"
+    @el1 = gets.chomp.to_i
+    puts "Input second element"
+    @el2 = gets.chomp.to_i
+  end
+
+  operators_list.each do |method_name, operator|
+    define_method method_name do
+      method_result = @el1.send(operator, @el2).to_f
+      "\e[#{rand(31..37)}m#{method_result}\e[0m"
+    end
+  end
+
+end
+
+result = MathOperations.new
+puts "Addition result is: #{result.plus}"
+puts "Substruction result is: #{result.minus}"
+puts "Division result is #{result.divide}"
+puts "Multiplication result is #{result.multiply}"
