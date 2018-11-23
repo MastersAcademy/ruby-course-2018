3a7d76e9ba264f0f752be9b1e9cf2055fd91dce3
Eugene <bekinassoff@gmail.com>
2018-11-23 17:27:53 +0200

    Changed values names and deleted an extra brackets

--- objects/eugene.doroshenko/task5.rb
+++ objects/eugene.doroshenko/task5.rb
@@ -1,27 +1,27 @@
 require_relative "colors"
 
 class ColorInputText
-  attr_accessor :text, :chosen_color
+  attr_accessor :input_text, :chosen_color
   extend ColorizeText
   
   def initialize
     puts "Please, input yor text"
-    @text = gets.chomp
+    @input_text = gets.chomp
     puts "choose color: red, green, blue or gray"
     @chosen_color = gets.chomp
   end
 
   colors_hash.each do |method_name, color|
     define_method method_name do
-      puts "\e[#{color}m#{@text}\e[0m"
+      puts "\e[#{color}m#{@input_text}\e[0m"
     end
   end
 
-  def argv_sym
-    send(@chosen_color.to_sym)
+  def color_text_sym
+    send @chosen_color.to_sym
   end
 
 end
 
-some_colored_text = ColorInputText.new
-puts some_colored_text.argv_sym
+some_colorized_text = ColorInputText.new
+puts some_colorized_text.color_text_sym
