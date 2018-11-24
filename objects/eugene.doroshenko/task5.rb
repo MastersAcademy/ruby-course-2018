5e58a2055ea1c25be74d9cb9c6aff865c829c4d5
Eugene <bekinassoff@gmail.com>
2018-11-24 10:46:26 +0200

    Added color validation

--- objects/eugene.doroshenko/task5.rb
+++ objects/eugene.doroshenko/task5.rb
@@ -2,23 +2,28 @@ require_relative "colors"
 
 class ColorInputText
   attr_accessor :input_text, :chosen_color
-  extend ColorizeText
+  include ColorizeText
   
   def initialize
     puts "Please, input yor text"
     @input_text = gets.chomp
     puts "choose color: red, green, blue or gray"
-    @chosen_color = gets.chomp
+    @chosen_color = gets.chomp.to_sym
   end
 
-  colors_hash.each do |method_name, color|
+  ColorizeText::ColorList.list_of_colors.each do |method_name, color|
     define_method method_name do
       puts "\e[#{color}m#{@input_text}\e[0m"
     end
   end
 
+  def color_present?
+   ColorizeText::ColorList.list_of_colors.key?(@chosen_color)
+  end
+
   def color_text_sym
-    send @chosen_color.to_sym
+    raise "there's no such color in hash" if color_present? == false
+    send @chosen_color
   end
 
 end
