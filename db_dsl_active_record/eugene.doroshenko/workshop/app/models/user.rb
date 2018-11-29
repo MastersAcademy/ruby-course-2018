b6b3c3d41d0cd75d9ac9d607c2cac26eb338d245
Eugene Doroshenko <bekinassoff@gmail.com>
2018-11-30 00:47:09 +0200

    added dependent: :destroy to user-photos association

--- db_dsl_active_record/eugene.doroshenko/workshop/app/models/user.rb
+++ db_dsl_active_record/eugene.doroshenko/workshop/app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ActiveRecord::Base
   has_many :galleries, dependent: :destroy
-  has_many :photos
+  has_many :photos, dependent: :destroy
 
   before_validation :strip_first_name
   validates :first_name, presence: true
