6cc9876e44afd6e624e43257e6da09cd53152fe0
Eugene <bekinassoff@gmail.com>
2018-11-29 14:50:09 +0200

    unused variable deleted

--- db_dsl_active_record/eugene.doroshenko/workshop/run.rb
+++ db_dsl_active_record/eugene.doroshenko/workshop/run.rb
@@ -4,7 +4,7 @@ require './config/initializers/database'
 
 User.create!(first_name: 'Doctor', last_name: 'Who')
 @photorapher = User.last
-@new_gallery = @photorapher.galleries.create!(gallery_name: 'First gallery')
+@photorapher.galleries.create!(gallery_name: 'First gallery')
 @last_gallery = Gallery.last
 @last_gallery.photos.create!(photo_name: 'First Photo', photo_desc: 'Just a test photo', photo_url: '/first-photo-url1.jpg')
 puts @photorapher.full_name
