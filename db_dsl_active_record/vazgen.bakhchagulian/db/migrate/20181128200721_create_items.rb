df10e8f4680e515dc27a2f5ecb73716e2b72d024
st3p <androidmarketchrome@gmail.com>
2018-12-03 17:19:20 +0200

    fixes

--- db_dsl_active_record/vazgen.bakhchagulian/db/migrate/20181128200721_create_items.rb
+++ db_dsl_active_record/vazgen.bakhchagulian/db/migrate/20181128200721_create_items.rb
@@ -2,9 +2,10 @@ class CreateItems < ActiveRecord::Migration[5.1]
   def up
     create_table :items
 
-    add_column :items, :user_id, :integer
+    # add_column :items, :user_id, :integer
     add_column :items, :name, :string
-    add_reference :item, :user
+
+    add_reference :item, :user, foreign_key: true
   end
 
   def down
