445c2199248092cf02e9fd19fe657339b3d7c6c3
TarasTurchenko <tarasturhchenko1551@gmail.com>
2018-12-13 18:20:15 +0200

    added validation for users and categories

--- db_dsl_active_record/taras.turchenko/app/models/category.rb
+++ db_dsl_active_record/taras.turchenko/app/models/category.rb
@@ -2,4 +2,6 @@ require 'active_record'
 
 class Category < ActiveRecord::Base
   has_and_belongs_to_many :movies
+
+  validates_presence_of :name
 end
--- db_dsl_active_record/taras.turchenko/app/models/user.rb
+++ db_dsl_active_record/taras.turchenko/app/models/user.rb
@@ -3,7 +3,6 @@ require 'active_record'
 class User < ActiveRecord::Base
   belongs_to :movie
 
-  validates :avatar, presence: true
-  validates :password, presence: true
-  validates :login, presence: true
+  validates_presence_of :password
+  validates_presence_of :login
 end
