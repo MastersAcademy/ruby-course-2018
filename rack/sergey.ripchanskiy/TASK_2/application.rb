044b9131388c9d0f7f61d6aa348bcf0ac9ccca2e
Sergey <sergeyripchanskiy@gmail.com>
2018-12-17 18:16:33 +0200

    create router.rb file, move route ligic to this class

--- rack/sergey.ripchanskiy/TASK_2/application.rb
+++ rack/sergey.ripchanskiy/TASK_2/application.rb
@@ -4,16 +4,6 @@ Dir.glob(app_files).each { |file| require(file) }
 class Application
   def call(env)
     request = Rack::Request.new(env)
-    find_pet_action(request)
-  end
-
-  def find_pet_action(request)
-    if request.path == '/'
-      BaseController.new(request).new_game
-    elsif request.path == '/start_game'
-      BaseController.new(request).start_game
-    else request.path == '/dogs/menu'
-      BaseController.new(request).menu
-    end
+    Router.new.find_pet_action(request)
   end
 end
