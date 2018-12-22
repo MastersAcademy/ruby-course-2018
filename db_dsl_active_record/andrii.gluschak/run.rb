7425fe75f40a22137053734c1b5e64f42ed6b44d
MrGaoler <notwronglife@gmail.com>
2018-12-22 17:58:12 +0200

    add changes to run.rb

--- db_dsl_active_record/andrii.gluschak/run.rb
+++ db_dsl_active_record/andrii.gluschak/run.rb
@@ -1,10 +1,11 @@
 require 'pry'
 require './config/initializers/database.rb'
 
-User.create!(first_name: 'Somebody', last_name: 'Anyone')
-Post.create!(title: 'head', body: 'text', user_id: '1')
-Image.create!(url:'test', imageable_id: '1', imageable_type: 'jpeg')
+user = User.create!(first_name: 'Somebody', last_name: 'Anyone')
+user.posts.create(title: 'head2', body: 'text2')
+user.images.create(url: 'test4')
 
 puts User.all.last.attributes
 puts Post.all.last.attributes
 puts Image.all.last.attributes
+
