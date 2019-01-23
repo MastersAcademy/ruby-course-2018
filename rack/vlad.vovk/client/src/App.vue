45be30f2d146a816db8acde0a4c8ebfa8092bec5
vladvovk2 <vlad.vovk.844@gmail.com>
2019-01-23 17:29:15 +0200

    Workshop

---
+++ rack/vlad.vovk/client/src/App.vue
@@ -0,0 +1,16 @@
+<template>
+    <div id="app">
+        <Critter/>
+    </div>
+</template>
+
+<script>
+    import Critter from './components/critter/Critter.vue'
+
+    export default {
+        name: 'app',
+        components: { Critter }
+    }
+</script>
+
+<style></style>
