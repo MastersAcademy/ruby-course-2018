c4974c43be6ab9e03a6be9eb5e98cb7d2aef692a
taras.turchenko <tarasturhchenko1551@gmail.com>
2019-01-27 20:11:55 +0200

    added updating movies and deleting

--- rails/taras.turchenko/lection_hw/app/controllers/movies_controller.rb
+++ rails/taras.turchenko/lection_hw/app/controllers/movies_controller.rb
@@ -6,11 +6,7 @@ class MoviesController < ApplicationController
   def new; end
 
   def create
-    attrs = declared(params).merge(
-      author: current_user,
-      description: params[:description],
-      category_ids: params[:category_ids]
-    )
+    attrs = declared(params).merge author: current_user
     movie = Movie.create! attrs
     redirect_to movie
   rescue
@@ -22,8 +18,6 @@ class MoviesController < ApplicationController
     @movie = Movie.find params[:id]
   end
 
-  def update; end
-
   def vote
     movie = Movie.find params[:id]
     begin
@@ -34,9 +28,38 @@ class MoviesController < ApplicationController
     redirect_to movie
   end
 
+  def edit
+    @movie = Movie.find params[:id]
+  end
+
+  def update
+    movie = Movie.find params[:id]
+    begin
+      movie.update! declared(params[:movie])
+      redirect_to movie
+    rescue
+      flash[:notice] = $ERROR_INFO
+      redirect_to edit_movie_path(movie)
+    end
+  end
+
+  def destroy
+    movie = Movie.find params[:id]
+    begin
+      movie.destroy!
+      redirect_to movies_path
+    rescue
+      flash[:notice] = $ERROR_INFO
+      redirect_to movie
+    end
+  end
+
   private
 
   def declared(params)
-    params.permit Movie::PROPERTIES
+    params.permit(Movie::PROPERTIES).merge(
+      description: params[:description],
+      category_ids: params[:category_ids]
+    )
   end
 end
