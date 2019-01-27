d07c8ec44dada84c85eb4a5fda3bcece86e11c70
Eugene Doroshenko <bekinassoff@gmail.com>
2019-01-27 19:09:28 +0200

    HW Workshop Rack

---
+++ rack/eugene.doroshenko/app/models/pet.rb
@@ -0,0 +1,94 @@
+class Pet
+  attr_reader  :hunger, :happiness, :sleepiness, :health
+  attr_accessor :event
+
+  MAX = 100
+  MIN = 0
+  STEP = 8
+
+  def initialize
+    self.health     = MAX
+    self.hunger     = MAX / 2
+    self.happiness  = MAX / 2
+    self.sleepiness = MAX / 2
+    self.event      = :idle
+  end
+
+  def rest
+    self.sleepiness += STEP
+    self.hunger     -= STEP
+    self.event       = :rest
+  end
+
+  def play
+    self.happiness  += STEP
+    self.hunger     -= STEP
+    self.sleepiness -= STEP
+    self.event      = :play
+  end
+
+  def feed
+    self.hunger     += STEP
+    self.happiness  += STEP
+    self.event      = :feed
+  end
+
+  def dead?
+    health.zero?
+  end
+
+  def health
+    if @health < 0
+      0
+    else
+      @health
+    end
+  end
+
+  def mood
+    case health
+    when 0..33
+      :sad
+    when 34..66
+      :worried
+    else
+      :happy
+    end
+  end
+
+  def to_json
+    {
+        health: health,
+        hunger: hunger,
+        happiness: happiness,
+        sleepiness: sleepiness,
+        mood: mood,
+        event: event,
+        dead: dead?
+    }
+  end
+
+  def hunger=(value)
+    @hunger = process(value)
+  end
+
+  def happiness=(value)
+    @happiness = process(value)
+  end
+
+  def sleepiness=(value)
+    @sleepiness = process(value)
+  end
+
+  def process(value)
+    if value < MIN
+      self.health -= STEP
+      MIN
+    elsif value > MAX
+      self.health -= STEP
+      MAX
+    else
+      value
+    end
+  end
+end
