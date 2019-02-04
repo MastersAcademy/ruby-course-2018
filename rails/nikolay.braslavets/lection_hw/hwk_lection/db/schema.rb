28ac3d2f4f0392914f52d473d97733287ba7b938
daglus <daglus.ck.ua@gmail.com>
2019-02-04 13:44:10 +0200

    Add field to user models. Create migration.

--- rails/nikolay.braslavets/lection_hw/hwk_lection/db/schema.rb
+++ rails/nikolay.braslavets/lection_hw/hwk_lection/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_29_143503) do
+ActiveRecord::Schema.define(version: 2019_02_04_114215) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -20,6 +20,8 @@ ActiveRecord::Schema.define(version: 2019_01_29_143503) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "first_name"
+    t.string "last_name"
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
