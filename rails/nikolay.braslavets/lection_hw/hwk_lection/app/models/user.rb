95f49471a9459622708c926d6c6c2eab8f051540
daglus <daglus.ck.ua@gmail.com>
2019-02-05 13:31:38 +0200

    Add validation in user model to new filed.

--- rails/nikolay.braslavets/lection_hw/hwk_lection/app/models/user.rb
+++ rails/nikolay.braslavets/lection_hw/hwk_lection/app/models/user.rb
@@ -1,5 +1,9 @@
 class User < ApplicationRecord
   has_secure_password
 
+  validates :first_name, :last_name, presence: true,
+            format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
+
   validates :email, presence: true, uniqueness: true
+
 end
