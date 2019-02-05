6b42f3eda46565a7ff0c93e9e83e1480d6b8c36f
daglus <daglus.ck.ua@gmail.com>
2019-02-05 14:51:22 +0200

    Add some code to user models from active record lessons.

--- rails/nikolay.braslavets/lection_hw/hwk_lection/app/models/user.rb
+++ rails/nikolay.braslavets/lection_hw/hwk_lection/app/models/user.rb
@@ -1,9 +1,21 @@
 class User < ApplicationRecord
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+
   has_secure_password
+
+  has_many :songs, dependent: :destroy
+  has_many :playlists, dependent: :destroy
+
+  before_validation :strip_first_name
 
   validates :first_name, :last_name, presence: true,
             format: { with: /\A[a-zA-Z]+\z/, message: "only allows letters" }
 
-  validates :email, presence: true, uniqueness: true
+  validates :email, presence: true, format: { with: VALID_EMAIL_REGEX }, uniqueness: true
 
+  private
+
+  def strip_first_name
+    self.first_name.strip! if first_name
+  end
 end
