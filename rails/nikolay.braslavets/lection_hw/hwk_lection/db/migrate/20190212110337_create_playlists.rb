98a3a08ee1ed718a4fb4196165545f061f86deb9
daglus <daglus.ck.ua@gmail.com>
2019-02-12 17:05:38 +0200

    Update schema.rb, fix playlist migration.

--- rails/nikolay.braslavets/lection_hw/hwk_lection/db/migrate/20190212110337_create_playlists.rb
+++ rails/nikolay.braslavets/lection_hw/hwk_lection/db/migrate/20190212110337_create_playlists.rb
@@ -2,7 +2,7 @@ class CreatePlaylists < ActiveRecord::Migration[5.2]
   def change
     create_table :playlists do |t|
       t.string :name
-      t.text :descriptoin
+      t.text :description
       t.references :user, foreign_key: true
 
       t.timestamps
--- rails/nikolay.braslavets/lection_hw/hwk_lection/db/schema.rb
+++ rails/nikolay.braslavets/lection_hw/hwk_lection/db/schema.rb
@@ -17,7 +17,7 @@ ActiveRecord::Schema.define(version: 2019_02_12_111013) do
 
   create_table "playlists", force: :cascade do |t|
     t.string "name"
-    t.text "descriptoin"
+    t.text "description"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
