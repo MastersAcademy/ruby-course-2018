172b5f918fc712e6b617cb6f4ec17d141e90640d
daglus <daglus.ck.ua@gmail.com>
2019-02-13 12:43:27 +0200

    Add playlist_id to Song model migration, add to permit in model.

--- rails/nikolay.braslavets/lection_hw/hwk_lection/app/controllers/songs_controller.rb
+++ rails/nikolay.braslavets/lection_hw/hwk_lection/app/controllers/songs_controller.rb
@@ -41,7 +41,7 @@ class SongsController < ApplicationController
   private
 
   def song_params
-    params.require(:song).permit(:artist, :name, :genre)
+    params.require(:song).permit(:artist, :name, :genre, :playlist_id)
   end
 
   def set_song
--- rails/nikolay.braslavets/lection_hw/hwk_lection/db/schema.rb
+++ rails/nikolay.braslavets/lection_hw/hwk_lection/db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_12_111013) do
+ActiveRecord::Schema.define(version: 2019_02_13_090120) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -31,6 +31,8 @@ ActiveRecord::Schema.define(version: 2019_02_12_111013) do
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer "playlist_id"
+    t.index ["playlist_id"], name: "index_songs_on_playlist_id"
     t.index ["user_id"], name: "index_songs_on_user_id"
   end
 
