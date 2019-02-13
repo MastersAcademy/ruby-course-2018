b16ae6429d3577010cc3ad16a3d3d07b41311c65
daglus <daglus.ck.ua@gmail.com>
2019-02-13 15:34:21 +0200

    Fix routes.rb

--- rails/nikolay.braslavets/workshop_hw/wan_password/config/routes.rb
+++ rails/nikolay.braslavets/workshop_hw/wan_password/config/routes.rb
@@ -4,7 +4,7 @@ Rails.application.routes.draw do
   resources :users
   resources :sessions, only: [:new, :create, :destroy]
 
-  get 'signup', to: 'users#new', as: 'signup'
-  get 'login', to: 'sessions#new', as: 'login'
-  get 'logout', to: 'sessions#destroy', as: 'logout'
+  get :signup, to: 'users#new', as: :signup
+  get :login, to: 'sessions#new', as: :login
+  get :logout, to: 'sessions#destroy', as: :logout
 end
