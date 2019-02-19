123eb2523fd93f2450cedc2d8b5a7483e5c6550b
Larioshin Boris <isakovbr@gmail.com>
2019-02-19 17:30:51 +0200

    add function in run.rb (workshop active record)

--- db_dsl_active_record/boris.larioshin/workshop_active_record/run.rb
+++ db_dsl_active_record/boris.larioshin/workshop_active_record/run.rb
@@ -1,11 +1,32 @@
 require 'pry'
+require 'faker'
 require './config/initializers/database.rb'
 
-User.create(first_name: 'Sherlock', last_name: 'Holmes', email: '221BBakerStreet1@london.uk', password: 'detective')
-User.create(first_name: 'John', last_name: 'Watson', email: '221BBakerStreet2@london.uk', password: 'doctor')
-User.create(first_name: 'James', last_name: 'Moriarty', email: '221BBakerStreet3@london.uk', password: 'professor')
-User.create(first_name: 'Mrs.', last_name: 'Hudson', email: '221BBakerStreet4@london.uk', password: 'landlady')
-User.create(first_name: 'Arthur ', last_name: 'Conan Doyle', email: '221BBakerStreet5@london.uk', password: 'writer')
-puts User.first.first_name
-puts User.first.last_name
-Playlist.create(title: 'Classic', description: "melody for violin", users_id: 1)
+# Generate users
+15.times do
+  User.create(
+      first_name: Faker::Name.first_name,
+      last_name: Faker::Name.last_name,
+      email: Faker::Internet.email,
+      password: Faker::Internet.password(8)
+  )
+end
+
+# Generate tracks
+50.times do
+  Track.create(
+      artist: Faker::Artist.name,
+      title: Faker::Lorem.sentence,
+      album: Faker::Music.album
+  )
+end
+
+#Add playlist for user
+10.times do |id|
+  User.find_by_id(id + 1).playlists.create(title: Faker::Lorem.word)
+end
+
+#Add track in playlist
+10.times do |id|
+  Playlist.find_by_id(id + 1).tracks << Track.find_by_id(rand(1..50))
+end
